c2e80e91f6055365947e9939dd4b394b2a8c203a
rayjay <526528945@qq.com>
2020-01-19 10:06:16 +0800

    abstract external modules

--- backend/web_bo/bin/globals.py
+++ backend/web_bo/bin/globals.py
@@ -3,7 +3,6 @@
 
 import configparser
 import logging
-import sys
 
 #######INIT LOG################
 logging.basicConfig(filename='../var/log/web_bo.log', format='%(process)d %(name)s %(asctime)s [%(levelname)s] %(filename)s:%(lineno)s %(funcName)s %(message)s',
@@ -13,6 +12,3 @@ g_logger = logging.getLogger('web_bo')
 
 g_cfg = configparser.ConfigParser()
 g_cfg.read('../etc/web_bo.ini')
-
-if '../lib' not in sys.path:
-    sys.path.append('../lib')

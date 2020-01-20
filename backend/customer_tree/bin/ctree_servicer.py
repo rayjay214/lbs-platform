ffa2fe73938aac633bf40eb738a86a1a3519ae05
rayjay <526528945@qq.com>
2020-01-20 14:16:47 +0800

    eid==0 means the customer is none

--- backend/customer_tree/bin/ctree_servicer.py
+++ backend/customer_tree/bin/ctree_servicer.py
@@ -13,7 +13,7 @@ class CTreeServicer(customer_tree_pb2_grpc.CTreeServicer):
 
     def getCustomerInfo(self, request, context):
         with self.tree_lock.gen_rlock():
-            if request.eid is not None:
+            if request.eid != 0:
                 node = search.find_by_attr(self.tree.root, name='id', value=request.eid)
             else:
                 node = search.find_by_attr(self.tree.root, name='login_name', value=request.login_name)

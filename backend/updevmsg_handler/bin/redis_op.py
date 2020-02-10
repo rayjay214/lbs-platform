c88a71d95218506ade544c5cba7765c85191585a
rayjay <526228945@qq.com>
2020-02-10 10:06:38 +0800

    update iccid

---
+++ backend/updevmsg_handler/bin/redis_op.py
@@ -0,0 +1,14 @@
+import redis
+
+class RedisOp():
+    def __init__(self, cfg):
+        self.host = cfg['host']
+        self.port = cfg['port']
+        self.redis = redis.StrictRedis(host=self.host, port=self.port, charset='utf-8', decode_responses=True)
+        self.pipe = self.redis.pipeline()
+
+    def getDeviceInfoById(self, dev_id):
+        dev_info = self.redis.hgetall('device:{}'.format(dev_id))
+        return dev_info
+
+
